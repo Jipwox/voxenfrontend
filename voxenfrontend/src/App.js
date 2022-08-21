a8dfbb0a482ae9da329147a567a34fdb1fd321ec
JP <johnphilip.cardi@gmail.com>
2022-08-20 19:36:02 -0700

    added app bar

--- voxenfrontend/src/App.js
+++ voxenfrontend/src/App.js
@@ -1,20 +1,35 @@
 import HomePage from "./home/HomePage";
 import SaucePage from "./sauce/SaucePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+
+const theme = createTheme();
 
 function App() {
   return (
-    <div>
-      <head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </head>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage/>}/>
-          <Route path="/sauce" element={<SaucePage/>} />
-        </Routes>
-      </Router>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div>
+        <head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </head>
+        <AppBar position="relative">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" noWrap>Voxenfree</Typography>
+          </Toolbar>
+        </AppBar>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/sauce" element={<SaucePage />} />
+          </Routes>
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
